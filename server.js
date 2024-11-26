338cb43f26acbf3645bc725be88285ab86b6bfed
leprecon73 lep <leprecon73@gmail.com>
2024-11-26 17:21:58 +0000

    38

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(function(req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(/*process.env.NODE_ENV==='test'*/1) {
+  if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
