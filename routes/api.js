ef7f0070852235406e9b3c047852f124cf34c13b
leprecon73 lep <leprecon73@gmail.com>
2024-11-26 18:51:06 +0000

    45

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 'use strict';
-let mongodb = require('mongodb');
+
+
 module.exports = function (app) {
   
   app.route('/api/threads/:board');
@@ -7,8 +8,8 @@ module.exports = function (app) {
   app.route('/api/replies/:board');
 
 };
-
+let mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://leprecon73:ZyNlSCV3B8kgJs1P@cluster0.y56hpsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 //ZyNlSCV3B8kgJs1P
