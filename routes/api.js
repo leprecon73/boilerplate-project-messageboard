489cd1827d42966c6629bb3efccf289692f6fc2f
leprecon73 lep <leprecon73@gmail.com>
2024-11-26 18:41:43 +0000

    40

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,6 @@ module.exports = function (app) {
 };
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://leprecon73:<db_password>@cluster0.y56hpsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://leprecon73:ZyNlSCV3B8kgJs1P@cluster0.y56hpsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 //ZyNlSCV3B8kgJs1P
