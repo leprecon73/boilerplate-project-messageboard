f226dda2414d6df0d67cb3664ceae8ced1e1e8d7
leprecon73 lep <leprecon73@gmail.com>
2024-11-26 18:14:20 +0000

    39

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 'use strict';
-/////
+
 module.exports = function (app) {
   
   app.route('/api/threads/:board');
@@ -7,3 +7,8 @@ module.exports = function (app) {
   app.route('/api/replies/:board');
 
 };
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://leprecon73:<db_password>@cluster0.y56hpsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+//ZyNlSCV3B8kgJs1P
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app = express();
   frameguard: { action: "sameorigin" }
 }));*/
 
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
