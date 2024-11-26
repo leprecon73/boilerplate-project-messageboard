75756b5245abf8351f5fcd235087436af4287fec
leprecon73 lep <leprecon73@gmail.com>
2024-11-26 20:38:26 +0000

    46

--- routes/api.js
+++ routes/api.js
@@ -8,8 +8,9 @@ module.exports = function (app) {
   app.route('/api/replies/:board');
 
 };
+require('dotenv').config();
+const URI = process.env.URI;
 let mongoose = require('mongoose');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://leprecon73:ZyNlSCV3B8kgJs1P@cluster0.y56hpsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-//ZyNlSCV3B8kgJs1P
+//const { MongoClient, ServerApiVersion } = require('mongodb');
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
