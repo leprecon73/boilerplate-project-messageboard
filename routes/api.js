fcbda3abcdc80db2873235e063b75f9af7256fd4
leprecon73 lep <leprecon73@gmail.com>
2024-11-27 08:13:16 +0000

    54

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,13 @@ const threadSchema = new mongoose.Schema({
   bumped_on: { type: Date, default: Date.now },
   reported: { type: Boolean, default: false },
   delete_password: String,
-  replies: []
+  replies: [    {
+    _id: { type: ObjectId, default: new ObjectId() },
+    text: String,
+    created_on: { type: Date, default: Date.now },
+    delete_password: String,
+    reported: { type: Boolean, default: false },
+  },]
 });
 const modelThread = mongoose.model('Thread', threadSchema) 
 
