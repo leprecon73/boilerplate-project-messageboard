033c71a03d5f17edc31ff448d56d9c551df4132c
leprecon73 lep <leprecon73@gmail.com>
2024-11-29 13:35:37 +0000

    107

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -34,4 +34,37 @@ suite('Functional Tests', function() {
         }
       };
 
+  /**Creating a new thread: POST request to /api/threads/{board} */
+  test("Creating a new thread: POST request to /api/threads/{board}", done => {
+    chai
+      .request(server)
+      .post("/api/threads/funcTest")
+      .send({
+        board: "funcTest",
+        text:  "testText",
+        delete_password: "passwordTest"
+      })
+      
+      .end(function (err, res) {
+         assert.equal(res.status, 200);
+        assert.equal(res.body.text, 'testText');
+        assert.equal(res.body.delete_password, 'passwordTest');
+        assert.equal(res.body.board, 'funcTest');
+        assert.equal(res.body.reported, false);
+        assert.isArray(res.body.replies);       
+        done();
+      });
+
+    done();
+  });
+/*  Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}
+Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password
+Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
+Reporting a thread: PUT request to /api/threads/{board}
+Creating a new reply: POST request to /api/replies/{board}
+Viewing a single thread with all replies: GET request to /api/replies/{board}
+Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password
+Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password
+Reporting a reply: PUT request to /api/replies/{board} */
+
 });
