e574088136af8ecd03a6684adf96cab9331dc1d1
leprecon73 lep <leprecon73@gmail.com>
2024-11-29 14:16:03 +0000

    119

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,232 +5,61 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-let board_of_thread_to_delete
-let thread_to_delete
-let pw_for_thread_to_delete
-let thread_url
-
-let reply_to_delete
-let pw_for_reply_to_delete
-
 suite('Functional Tests', function() {
-  // #1
-  test('#1 Creating a new thread: POST request to /api/threads/{board}', function (done) {
+   /* async () => {
+        const date = new Date();
+        const text = `fcc_test_${date}`;
+        const deletePassword = 'delete_me';
+        const data = { text, delete_password: deletePassword };
+        const url = 'https://3000-leprecon73-boilerplatep-klxzfe8egje.ws-eu116.gitpod.io';
+        const res = await fetch(url + '/api/threads/fcc_test', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data)
+        });
+        if (res.ok) {
+          const checkData = await fetch(url + '/api/threads/fcc_test');
+          const parsed = await checkData.json();
+          try {
+            assert.equal(parsed[0].text, text);
+            assert.isNotNull(parsed[0]._id);
+            assert.equal(new Date(parsed[0].created_on).toDateString(), date.toDateString());
+            assert.equal(parsed[0].bumped_on, parsed[0].created_on);
+            assert.isArray(parsed[0].replies);
+          } catch (err) {
+            throw new Error(err.responseText || err.message);
+          }
+        } else {
+          throw new Error(`${res.status} ${res.statusText}`);
+        }
+      };*/
+
+  /**Creating a new thread: POST request to /api/threads/{board} */
+  test("Creating a new thread: POST request to /api/threads/{board}", done => {
     chai
       .request(server)
-      .post('/api/threads/test%20board%202') // did not write :board to get FCC tests to work
+      .post("/api/threads/funcTest")
       .send({
-        board: 'test board 2',
-        text: 'test thread title/text',
-        delete_password: 'pw'
+        board: "funcTest",
+        text:  "testText",
+        delete_password: "passwordTest"
       })
-      .end(function (err, res) {
-        //console.log(res.body)
-        board_of_thread_to_delete = res.body.board
-        thread_to_delete = res.body._id;
-        pw_for_thread_to_delete = res.body.delete_password;
-        
+      
+      .end( (err, res) => {
         assert.equal(res.status, 200);
-        assert.equal(res.body.text, 'test thread title/text');
-        assert.equal(res.body.delete_password, 'pw');
-        assert.equal(res.body.board, 'test board 2');
-        assert.typeOf(res.body.created_on, 'string');
-        assert.typeOf(res.body.bumped_on, 'string'); 
-        assert.equal(res.body.reported, false);
-        assert.isArray(res.body.replies);       
         done();
       });
+
+    done();
   });
-
-  // #2
-  test('#2 Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}', function (done) {
-    chai
-      .request(server)
-      .get('/api/threads/test%20board')
-      .query({})
-      .end(function (err, res) {
-        //console.log(res.body)
-        assert.equal(res.status, 200);
-        assert.isArray(res.body);
-        assert.equal(res.body.length, 10);
-        assert.isBelow(res.body[0].replies.length, 4);
-        assert.isBelow(res.body[1].replies.length, 4);
-        assert.isBelow(res.body[2].replies.length, 4);
-        assert.isBelow(res.body[3].replies.length, 4);
-        assert.isBelow(res.body[4].replies.length, 4);
-        assert.isBelow(res.body[5].replies.length, 4);
-        assert.isBelow(res.body[6].replies.length, 4);
-        assert.isBelow(res.body[7].replies.length, 4);
-        assert.isBelow(res.body[8].replies.length, 4);
-        assert.isBelow(res.body[9].replies.length, 4);
-        done();
-      });
-  });
-
-  // #3
-  test('#3 Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password', function (done) {
-    chai
-      .request(server)
-      .delete('/api/threads/:board')
-      .send({
-        board: board_of_thread_to_delete,
-        thread_id: thread_to_delete,
-        delete_password: 'incorrect password'
-      })
-      .end(function (err, res) {
-        //console.log(res.body)
-        assert.equal(res.status, 200);
-        assert.equal(res.body, 'incorrect password');
-        done();
-      });
-  });
-
-  // #5
-  test('#5 Reporting a thread: PUT request to /api/threads/{board}', function (done) {
-    chai
-      .request(server)
-      .put('/api/threads/:board')
-      .send({
-        board: board_of_thread_to_delete,
-        thread_id: thread_to_delete
-      })
-      .end(function (err, res) {
-        //console.log('PUT request res.body: ', res.body)
-        assert.equal(res.status, 200);
-        assert.equal(res.body, 'success');
-        done();
-      });
-  });
-
-  // #6
-  test('#6 Creating a new reply: POST request to /api/replies/{board}', function (done) {
-      chai
-      .request(server)
-      .post('/api/replies/test%20board%202') // did not write :board to get FCC tests to work
-      .send({
-        board: board_of_thread_to_delete,
-        thread_id: thread_to_delete,
-        text : 'some reply text',
-        delete_password: pw_for_thread_to_delete
-      })
-      .end(function (err, res) {
-        console.log(res.body)
-        reply_to_delete = res.body.replies[0]._id;
-        pw_for_reply_to_delete = res.body.replies[0].delete_password;
-        
-        assert.equal(res.status, 200);
-        assert.equal(res.body.text, 'test thread title/text');
-        assert.equal(res.body.delete_password, 'pw');
-        assert.equal(res.body.board, 'test board 2');
-        assert.typeOf(res.body.created_on, 'string');
-        assert.typeOf(res.body.bumped_on, 'string'); 
-        assert.equal(res.body.reported, true);
-        assert.isArray(res.body.replies);    
-        assert.equal(res.body.replies[0].text, 'some reply text');
-        assert.equal(res.body.replies[0].delete_password, 'pw');  
-        assert.typeOf(res.body.replies[0].created_on, 'string'); 
-        assert.typeOf(res.body.replies[0].reported, 'boolean');  
-        assert.isNotNull(res.body.replies[0]._id); 
-        done();
-      });
-  });
-
-  // #7
-  test('#7 Viewing a single thread with all replies: GET request to /api/replies/{board}', function (done) {
-    chai
-      .request(server)
-      .get('/api/replies/test%20board?thread_id=618bc0d05994b431c4b1c530')
-      .query({})
-      .end(function (err, res) {
-        console.log(res.body)
-        assert.equal(res.status, 200);
-        assert.isArray(res.body.replies);
-        assert.typeOf(res.body.replies[0].created_on, 'string');
-        assert.typeOf(res.body.replies[0]._id, 'string');
-        assert.isUndefined(res.body.replies[0].reported);
-        assert.isUndefined(res.body.replies[0].delete_password);
-        done();
-      });
-  });
-
-  // #8
-  test('#8 Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password', function (done) {
-    chai
-      .request(server)
-      .delete('/api/replies/:board')
-      .send({
-        board: board_of_thread_to_delete,
-        thread_id: thread_to_delete,
-        reply_id: reply_to_delete,
-        delete_password: 'incorrect password'
-      })
-      .end(function (err, res) {
-        //console.log(res.body)
-        assert.equal(res.status, 200);
-        assert.equal(res.body, 'incorrect password');
-        done();
-      });
-  });
-
-  // #10
-  test('#10 Reporting a reply: PUT request to /api/replies/{board}', function (done) {
-    chai
-      .request(server)
-      .put('/api/replies/:board')
-      .send({
-        board: board_of_thread_to_delete,
-        thread_id: thread_to_delete,
-        reply_id: reply_to_delete
-      })
-      .end(function (err, res) {
-        //console.log('PUT request res.body: ', res.body)
-        assert.equal(res.status, 200);
-        assert.equal(res.body, 'success');
-        done();
-      });
-  });
-
-//Added these two tests at the end because the ones above depend on the thread and reply created (and the two tests below delete these)
-  // #9
-  test('#8 Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password', function (done) {
-    chai
-      .request(server)
-      .delete('/api/replies/:board')
-      .send({
-        board: board_of_thread_to_delete,
-        thread_id: thread_to_delete,
-        reply_id: reply_to_delete,
-        delete_password: pw_for_reply_to_delete
-      })
-      .end(function (err, res) {
-        //console.log(res.body)
-        assert.equal(res.status, 200);
-        assert.equal(res.body, 'success');
-        done();
-      });
-  });
-
-  // #4
-  test('#4 Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password', function (done) {
-    chai
-      .request(server)
-      .delete('/api/threads/:board')
-      .send({
-        board: board_of_thread_to_delete,
-        thread_id: thread_to_delete,
-        delete_password: pw_for_thread_to_delete
-      })
-      .end(function (err, res) {
-        //console.log(res.body)
-        assert.equal(res.status, 200);
-        assert.equal(res.body, 'success');
-        done();
-      });
-  });
-
-
-
-
-
+/*  Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}
+Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password
+Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
+Reporting a thread: PUT request to /api/threads/{board}
+Creating a new reply: POST request to /api/replies/{board}
+Viewing a single thread with all replies: GET request to /api/replies/{board}
+Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password
+Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password
+Reporting a reply: PUT request to /api/replies/{board} */
 
 });
