f9777449448c0bb3e415799433f694f1db9b32da
leprecon73 lep <leprecon73@gmail.com>
2024-12-01 17:15:00 +0000

    228

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -169,6 +169,7 @@ test('7.Viewing a single thread with all replies: GET request to /api/replies/{b
           assert.equal(res.status, 200);
           assert.isArray(res.body.replies);
           assert.property(res.body.replies[0], 'text');
+          assert.property(res.body.replies[0], 'delete_password');
           console.log("res.body.replies =", res.body.replies);
           console.log("res.body =", res.body);
           /*replyId = res.body.replies[0]._id;*/
