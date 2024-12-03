d60a81efc00cc6aadb432082ac10db6cadb7d101
leprecon73 <41924221+leprecon73@users.noreply.github.com>
2024-12-03 13:35:07 +0000

    306

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(function (req, res, next) {
 });
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 40001, function () {
   console.log("Your app is listening on port " + listener.address().port);
   if (/*process.env.NODE_ENV==='test'*/ true) {
     console.log("Running Tests...");
